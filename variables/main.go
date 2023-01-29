3eccb29ed10671ab36c481e6ec68e287d2eac559
Hussein Omar <husseinomar6190@gmail.com>
2023-01-29 18:57:01 +0300

    ways to declaring a variables in go

--- variables/main.go
+++ variables/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("hello")
+
+	// Declaring a variable without assigning
+	var x int
+// Assigning the same variable 
+	x = 15
+
+	// Declaring a variable and assigning at the same time
+	var y int = 34
+
+	// Declaring a variable using the shorthand expression
+	z := 45
+	fmt.Printf("x: %v ", x)
+	fmt.Printf("x: %v ", y)
+	fmt.Printf("x: %v ", z)
 }
