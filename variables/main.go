d151e7d7ac33ca2da6b895e9d4481ae9573a5489
Hussein Omar <husseinomar6190@gmail.com>
2023-01-29 19:51:31 +0300

    Declaring variables outside scope of main function

--- variables/main.go
+++ variables/main.go
@@ -2,19 +2,31 @@ package main
 
 import "fmt"
 
+// Declaring variables outside scope of main function
+// You can't use the shorthand notation
+var name string = "john"
+
+// Declaring multiple variables at once
+var (
+		age int64 = 56
+		gender string = "M"
+)
+
 func main()  {
 
 	// Declaring a variable without assigning
 	var x int
+
 // Assigning the same variable 
 	x = 15
 
 	// Declaring a variable and assigning at the same time
 	var y int = 34
 
-	// Declaring a variable using the shorthand expression
+	// Declaring a variable using the shorthand notation
 	z := 45
-	fmt.Printf("x: %v ", x)
-	fmt.Printf("x: %v ", y)
-	fmt.Printf("x: %v ", z)
+	fmt.Printf("x: %v, %T ", x, x)
+	fmt.Printf("y: %v ", y)
+	fmt.Printf("z: %v ", z)
+	fmt.Printf("Name: %v ", name)
 }
