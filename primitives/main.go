95b62fc2a3ff5cd5a600b0005fde71872f4dcf30
Husssein Omar <husseinomar6190@gmail.com>
2023-02-05 23:04:00 +0300

    Bit Shifting, arithmetic operations & Bit Operators

--- primitives/main.go
+++ primitives/main.go
@@ -28,6 +28,27 @@ func main()  {
 		var z int64 = 1
   fmt.Printf("%v, %T\n", z, z)
 
-		
+		// Basic arithmetic operations built into Golang
+				// Only Integers of the same type can be arithmetic operations be done
+
+		fmt.Println(8 + 9)
+		fmt.Println(10 - 9)
+		fmt.Println(8 * 9)
+		fmt.Println(8/2)
+		fmt.Println(10 % 3)
+
+		// The Bit Operators
+		a := 10
+		b := 3
+		fmt.Println (a & b) // 0010
+		fmt.Println(a | b) // 1011
+		fmt.Println(a^ b) // 1001
+		fmt.Println(a &^ b)// 0100
+
+		// Bit Shifting
+		f := 8 // 2^3
+		fmt.Println (f << 3) // 2^3* 2^3 = 2^6
+		fmt.Println(f >> 3) // 2^3 / 2^3 = 2^0
+
 
 }
