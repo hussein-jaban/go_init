269b38c4cd958aee144a6a2f56c04bcd327b905a
Husssein Omar <husseinomar6190@gmail.com>
2023-02-06 22:49:22 +0300

    booleans and interger types

--- primitives/main.go
+++ primitives/main.go
@@ -4,8 +4,30 @@ import (
 	"fmt"
 )
 
+// Primitive Data Types
 
 func main()  {
-	j := 9
+
+	// Booleans Types
+	var	j bool = true
 	fmt.Printf("%v, %T\n", j, j)
+
+	// When initialization a variable without assign a value, go assigns a zero value which is falsly in nature.
+	var c bool
+		fmt.Printf("%v, %T\n", c, c)
+
+		// Numeric Types in Go
+
+		// Integers
+		var i int = 1
+  fmt.Printf("%v, %T\n", i, i)
+		var q int8 = 1
+  fmt.Printf("%v, %T\n", q, q)
+		var w int32 = 1
+  fmt.Printf("%v, %T\n", w, w)
+		var z int64 = 1
+  fmt.Printf("%v, %T\n", z, z)
+
+		
+
 }
