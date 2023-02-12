9d77592146fff5bf216680af040c170649529370
Husssein Omar <husseinomar6190@gmail.com>
2023-02-12 14:24:21 +0300

    shodowing constants

--- constants/main.go
+++ constants/main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 )
 
+const a int16 = 27
 
 func main()  {
 	const myConst int = 23
 	fmt.Printf("%v, %T\n", myConst, myConst) // 23, int
+
+	const a int = 14
+ fmt.Printf("%v, %T\n", a, a) // 14, int
 } 
