6859f4d5d615ed55b8f73a87e51b0861a6051000
Husssein Omar <husseinomar6190@gmail.com>
2023-02-12 14:49:32 +0300

    enumerated contants aka iota

--- constants/main.go
+++ constants/main.go
@@ -6,6 +6,18 @@ import (
 
 const a int16 = 27
 
+// Using iota with constants
+const (
+				x = iota
+				y
+				z
+)
+
+// Iota resets to 0
+const (
+			x2 = iota
+)
+
 func main()  {
 	const myConst int = 23
 	fmt.Printf("%v, %T\n", myConst, myConst) // 23, int
@@ -18,4 +30,9 @@ func main()  {
 	const c = 42
 	var b int16 = 27
 	fmt. Printf("%v, %T\n", c + b, c + b) // 69, int16
+
+	fmt.Printf("%v\n", x) // 0
+	fmt.Printf("%v\n", y) // 1
+	fmt.Printf("%v\n", z) // 2
+	fmt.Printf("%v\n", x2) // 0
 } 
