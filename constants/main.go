512ff28134ecf827d926c308af7c1a5ea68fc54f
Husssein Omar <husseinomar6190@gmail.com>
2023-02-12 14:37:39 +0300

    Constants type inference

--- constants/main.go
+++ constants/main.go
@@ -10,6 +10,12 @@ func main()  {
 	const myConst int = 23
 	fmt.Printf("%v, %T\n", myConst, myConst) // 23, int
 
+	// Shadowing
 	const a int = 14
  fmt.Printf("%v, %T\n", a, a) // 14, int
+
+	// Constants type inference
+	const c = 42
+	var b int16 = 27
+	fmt. Printf("%v, %T\n", c + b, c + b) // 69, int16
 } 
