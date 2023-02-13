1a2d9352c06d4a3a8895e6a16def6047167ed764
Husssein Omar <husseinomar6190@gmail.com>
2023-02-13 21:24:01 +0300

    slices

--- arrays-&-slices/main.go
+++ arrays-&-slices/main.go
@@ -6,7 +6,7 @@ import (
 
 func main()  {
 	// Arrays
-	grades :=[3]int{97, 85, 93}
+	grades := [3]int{97, 85, 93}
  fmt.Printf("Grades: %v\n", grades) // Grades: [97 85 93]
 
 	// Using the lateral syntax to declare arrays
@@ -19,4 +19,20 @@ func main()  {
 	students[1] = "john"
 	students[2] = "ali"
 	fmt.Printf("Students: %v\n", students) // Students: [hussein john ali]
+
+	// Slices
+	z := []int{97, 85, 93}
+	fmt.Printf("Z: %v\n", z) // Z: [97 85 93]
+
+	// create slice using the brackests syntax []
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := a[:] // slice of all elements
+	c := a[3:] // slice from 4th element to end
+	d := a[:6] // slice first 6 elements
+	e := a[3:6] // slice the 4th, 5th, and 6th elements
+	fmt.Println(a) // [1 2 3 4 5 6 7 8 9 10]
+	fmt.Println(b) // [1 2 3 4 5 6 7 8 9 10]
+	fmt.Println(c) // [4 5 6 7 8 9 10]
+	fmt.Println(d) // [1 2 3 4 5 6]
+	fmt.Println(e) // [4 5 6]
 } 
