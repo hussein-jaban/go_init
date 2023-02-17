3894379026393740f9865fc6e48bcb181639a7b8
Husssein Omar <husseinomar6190@gmail.com>
2023-02-17 13:36:32 +0300

    Creating map with the make function

--- maps-&-structs/main.go
+++ maps-&-structs/main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main()  {
+	// Creating Map
 	statePopulation := map[string]int{
 		"ohio": 345656,
 		"columbus": 36457537,
 		"new york": 98798688,
 	}
 	fmt.Println(statePopulation) // map[columbus:36457537 new york:98798688 ohio:345656]
+
+	// Creating map with the make function
+	make_map := make(map[string]int)
+	make_map = map[string]int{
+			"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	fmt.Println(make_map) // map[a:1 b:2 c:3]
 }
