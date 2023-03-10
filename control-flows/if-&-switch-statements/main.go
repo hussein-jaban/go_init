6fa135a0a67207d097958326ac4bc5d06b905b82
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-03-10 20:37:22 +0300

    The initializer syntax

--- control-flows/if-&-switch-statements/main.go
+++ control-flows/if-&-switch-statements/main.go
@@ -14,6 +14,19 @@ func main()  {
 
 		if false {
 			fmt.Println("This will not be printed")
-		
+		}
+
+		// The initializer syntax with if, ie pattern matching in elixir
+		statePopulations := map[string]int{
+				"California": 39250017,
+				"Texas": 27862596,
+				"Florida": 20612439,
+				"New York": 19745289,
+				"Pennsylvania": 12802503,
+				"Illinois": 12801539,
+				"Ohio": 11614373,
+				}
+		if pop, ok := statePopulations["Florida"]; ok {
+					fmt.Println (pop)
 		}
 }
